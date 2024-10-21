55cda493444a3239de502c02ddad771e420e9ea3
Bhavye Mathur <bhavyemathur@gmail.com>
2024-10-21 13:13:16 -0400

    Added image resizing code to Dataset

--- datasets/datasets.py
+++ datasets/datasets.py
@@ -30,9 +30,10 @@ class KeyboardDataset(Dataset):
 class BinaryKeyboardSegmentationDataset(Dataset):
     _to_image = transforms.ToImage()
 
-    def __init__(self, _imgs, device="cpu"):
+    def __init__(self, _imgs, size=None, device="cpu"):
         self._device = device
 
+        self._resize = identity if size is None else transforms.Resize(size)
         self._images = list(tqdm(map(self._get_img, _imgs), total=len(_imgs)))
         self._masks = list(tqdm(map(self._get_mask, _imgs), total=len(_imgs)))
 
@@ -56,11 +57,11 @@ class BinaryKeyboardSegmentationDataset(Dataset):
         return len(self._images)
 
     def _get_img(self, _img: str):
-        return self._to_image(Image.open(_img)).to(self._device)
+        return self._resize(self._to_image(Image.open(_img))).to(self._device)
 
     def _get_mask(self, _img: str):
         _img = Image.open(f"blender/masks/keyboard_{os.path.basename(_img)[:3]}.png").convert("RGB")
-        return self._to_image(_img).to(self._device)
+        return self._resize(self._to_image(_img)).to(self._device)
 
 
 __all__ = ["KeyboardDataset", "BinaryKeyboardSegmentationDataset"]
