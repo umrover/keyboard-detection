07609bdf6eca9c293bd01b99a0b3787bad6b1ef5
Bhavye Mathur <bhavyemathur@gmail.com>
2024-10-24 18:18:39 -0400

    Split Blender scripts into more organised files

--- blender/random_viewpoints.py
+++ blender/random_viewpoints.py
@@ -1,8 +1,11 @@
 import bpy
-from mathutils import Vector, Matrix
+from mathutils import Vector
 from random import uniform
 from numpy import random
-from math import radians, degrees, cos, acos, sin
+from math import acos, degrees
+
+math_utils = bpy.data.texts["math_utils"].as_module()
+blender_utils = bpy.data.texts["blender_utils"].as_module()
 
 # PARAMETERS CONFIGURATION
 # --------------------------------------------
@@ -26,145 +29,40 @@ LOCATION_SAMPLING = "gaussian"
 THETA_SAMPLING = "gaussian"
 PHI_SAMPLING = "gaussian"
 
-
 # SCRIPT BEGINS HERE
 # --------------------------------------------
 
-def get_child_by_name(collection, name):
-    for child in collection.all_objects:
-        if child.name.startswith(name):
-            return child
-
-
-# See https://blender.stackexchange.com/questions/27491/python-vertex-normal-according-to-world
-def get_object_normal(obj):
-    normal = obj.data.vertices[0].normal.to_4d()
-    normal.w = 0
-    normal = (obj.matrix_world @ normal).to_3d()
-    return normal / normal.length
-
-
-# See https://blender.stackexchange.com/questions/169672/object-hide-render-crashes-blender-before-starting-to-bake
-def get_collection_objects(collection):
-    obj_names = [obj.name for obj in collection.all_objects]
-
-    for name in obj_names:
-        obj = bpy.data.objects.get(name)
-        yield obj
-
-
-def hide_collection(collection, val=True, exclude=()):
-    for obj in get_collection_objects(collection):
-        if obj in exclude:
-            continue
-
-        obj.hide_render = val
-        obj.hide_set(val)
-
-
 for i in range(TOTAL_KEYBOARDS):
     objs = bpy.data.collections[f"Keyboard {i + 1}"]
-    hide_collection(objs, True)
+    blender_utils.hide_collection(objs, True)
 
 keyboard = bpy.data.collections[f"Keyboard {keyboard}"]
-keyboard_plane = get_child_by_name(keyboard, "Orientation")
+keyboard_plane = blender_utils.get_child_by_name(keyboard, "Orientation")
 camera = bpy.data.objects["Camera"]
 
-hide_collection(keyboard, False, exclude=(keyboard_plane,))
+blender_utils.hide_collection(keyboard, False, exclude=(keyboard_plane,))
+blender_utils.delete_empty_objects()
 
 # Adjusting theta lims to account for orientation vector
 
-normal = get_object_normal(keyboard_plane)
-angle = acos(normal.dot(Vector((0, 0, 1))))
-angle = degrees(angle)
+normal = blender_utils.get_plane_normal(keyboard_plane)
+angle = math_utils.angle_between(normal, Vector((0, 0, 1)))
 theta_lims = (theta_lims[0] - angle, theta_lims[1] - angle)
-
-
-def get_random_arc_angle(theta1, theta2, phi1, phi2):
-    theta1 = radians(theta1)
-    theta2 = radians(theta2)
-    phi1 = radians(phi1)
-    phi2 = radians(phi2)
-
-    if THETA_SAMPLING == "uniform":
-        theta = uniform(theta1, theta2)
-
-    elif THETA_SAMPLING == "gaussian":
-        theta = random.normal((theta1 + theta2) / 2, theta_std)
-        # theta = max(theta1, min(theta2, theta))
-
-    else:
-        raise ValueError("Unknown Theta Sampling")
-
-    # x1 = (1 - cos(phi1)) / 2
-    # x2 = (1 - cos(phi2)) / 2
-    # x = uniform(x1, x2)
-    # phi = arccos(1 - 2 * x)
-
-    # The above code simplifies to
-
-    x1 = cos(phi1)
-    x2 = cos(phi2)
-
-    if PHI_SAMPLING == "uniform":
-        x = uniform(x1, x2)
-
-    elif PHI_SAMPLING == "gaussian":
-        x = random.normal((x1 + x2) / 2, phi_std)
-        # x = max(-1, min(1, x))
-        x = max(x2, min(x1, x))
-
-    else:
-        raise ValueError("Unknown Phi Sampling")
-
-    phi = acos(x)
-
-    return theta, phi
-
-
-def spherical_to_cartesian(rho, theta, phi):
-    x = rho * sin(phi) * cos(theta)
-    y = rho * sin(phi) * sin(theta)
-    z = rho * cos(phi)
-
-    return z, y, x
-
-
-def point_at(camera, target, roll=0):
-    direction = target - camera.location
-    tracker, rotator = (("-Z", "Y"), "Z")
-    quat = direction.to_track_quat(*tracker)
-
-    quat = quat.to_matrix().to_4x4()
-    roll = radians(roll)
-    roll_matrix = Matrix.Rotation(roll, 4, rotator)
-
-    loc = camera.location.to_tuple()
-    camera.matrix_world = quat @ roll_matrix
-    camera.location = loc
-
-    return direction
-
-
-def delete_empty_objects():
-    objs = bpy.data.objects
-
-    for obj in bpy.context.scene.objects:
-        if obj.name.startswith("Empty"):
-            objs.remove(objs[obj.name], do_unlink=True)
-
-    bpy.ops.object.delete()
-
-
-delete_empty_objects()
 
 bpy.context.scene.frame_start = 1
 bpy.context.scene.frame_end = N
 
+
+def get_random_angle():
+    return math_utils.get_random_arc_angle(*theta_lims, *phi_lims,
+                                           THETA_SAMPLING, PHI_SAMPLING,
+                                           theta_std, phi_std)
+
+
 for i in range(N):
-    random_angle = get_random_arc_angle(*theta_lims, *phi_lims)
+    random_angle = get_random_angle()
     random_dist = uniform(*rho_lims)
-    random_vec = spherical_to_cartesian(random_dist, *random_angle)
+    random_vec = math_utils.spherical_to_cartesian(random_dist, *random_angle)
     random_vec = Vector(random_vec)
 
     if LOCATION_SAMPLING == "gaussian":
@@ -187,7 +85,7 @@ for i in range(N):
     # Create Camera Keyframe
 
     camera.location = random_vec
-    point_at(camera, random_translation, random_roll)
+    blender_utils.point_at(camera, random_translation, random_roll)
 
     camera.keyframe_insert(data_path="location", frame=i)
     camera.keyframe_insert(data_path="rotation_euler", frame=i)
@@ -203,5 +101,3 @@ for i in range(N):
 
     mesh.from_pydata([random_translation, random_vec], [(0, 1)], [])
     mesh.update()
-
-    # bpy.ops.object.empty_add(type="SPHERE", radius=0.05, align="WORLD", location=random_vec, scale=(1, 1, 1))
--- blender/segmentation_mask_v2.py
+++ blender/segmentation_mask_v2.py
@@ -1,86 +1,44 @@
 import bpy
 from mathutils import Vector
 
+math_utils = bpy.data.texts["math_utils"].as_module()
+blender_utils = bpy.data.texts["blender_utils"].as_module()
+
 # PARAMETERS CONFIGURATION
 # --------------------------------------------
 
 
-SET_MASK = False
+SET_MASK = True
 
-keyboard = 1
+keyboard = 2
 
 TOTAL_KEYBOARDS = 4
-
+K = 2
 
 # SCRIPT BEGINS HERE
 # --------------------------------------------
 
 
-# See https://blender.stackexchange.com/questions/169672/object-hide-render-crashes-blender-before-starting-to-bake
-def get_collection_objects(collection):
-    obj_names = [obj.name for obj in collection.all_objects]
-
-    for name in obj_names:
-        obj = bpy.data.objects.get(name)
-        yield obj
-
-
-def hide_collection(collection, val=True, exclude=()):
-    for obj in get_collection_objects(collection):
-        if obj in exclude:
-            continue
-
-        obj.hide_render = val
-        obj.hide_set(val)
-
-
-def get_children_by_name(collection, name, all=True):
-    objs = []
-
-    for child in collection.all_objects:
-        if child.name.startswith(name):
-            if child.data is None:
-                continue
-
-            if all:
-                objs.append(child)
-                continue
-
-            return child
-
-    return objs
-
-
-# See https://blender.stackexchange.com/questions/27491/python-vertex-normal-according-to-world
-def get_object_normal(obj):
-    normal = obj.data.vertices[0].normal.to_4d()
-    normal.w = 0
-    normal = (obj.matrix_world @ normal).to_3d()
-    return normal / normal.length
-
-
 for i in range(TOTAL_KEYBOARDS):
     objs = bpy.data.collections[f"Keyboard {i + 1}"]
-    hide_collection(objs, True)
+    blender_utils.hide_collection(objs, True)
 
 keyboard = bpy.data.collections[f"Keyboard {keyboard}"]
-orientation = get_children_by_name(keyboard, "Orientation", all=False)
-keys = get_children_by_name(keyboard, "Key")
-keys += get_children_by_name(keyboard, "Numpad")
+orientation = blender_utils.get_child_by_name(keyboard, "Orientation")
+keys = blender_utils.get_children_by_name(keyboard, "Key")
+keys += blender_utils.get_children_by_name(keyboard, "Numpad")
 
-hide_collection(keyboard, False)
-hide_collection(keyboard, SET_MASK, exclude=keys)
+blender_utils.hide_collection(keyboard, False)
+blender_utils.hide_collection(keyboard, SET_MASK, exclude=keys)
 
 orientation.hide_set(True)
 orientation.hide_render = True
 
-k = 2
-
 
 def get_ith_color(i):
-    r = i * k
-    g = k * (r // 255)
-    b = k * (g // 255)
+    r = K * i
+    g = K * (r // 255)
+    b = K * (g // 255)
 
     return r % 255, g % 255, b % 255
 
@@ -112,28 +70,18 @@ def create_mask_material(i):
     return mat
 
 
-def set_obj_material(obj, index, mat):
-    if len(obj.data.materials) == index:
-        obj.data.materials.append(mat)
-
-    obj.data.materials[index] = mat
-
-
 black = create_mask_material(0)
 
-z = Vector((0, 0, 1))
-orientation = get_object_normal(orientation)
-print(orientation)
+orientation = blender_utils.get_plane_normal(orientation)
 
-# Set Keys Material
 for i, ob in enumerate(keys):
     assert i <= 255
     print(ob)
 
     mat = create_mask_material(i + 1)
 
-    set_obj_material(ob, 1, black)
-    set_obj_material(ob, 2, mat)
+    blender_utils.set_obj_material(ob, 1, black)
+    blender_utils.set_obj_material(ob, 2, mat)
 
     for face in ob.data.polygons:
         if not SET_MASK:
