089f66305ca5c7772466ed2432ae516ac8b99886
Bhavye Mathur <bhavyemathur@gmail.com>
2024-10-30 23:15:42 -0400

    Added YOLO model notebook

--- datasets/binary_segmentation.py
+++ datasets/binary_segmentation.py
@@ -3,6 +3,7 @@ from tqdm.notebook import tqdm
 from multiprocessing import Pool
 
 from PIL import Image
+import numpy as np
 
 import torch
 from torch.utils.data import Dataset
@@ -14,13 +15,16 @@ from .internal import get_mask_path, identity
 class BinaryKeyboardSegmentationDataset(Dataset):
     _to_image = transforms.ToImage()
 
-    def __init__(self, paths: Sequence[str], size: tuple[float, float] | None = None):
+    def __init__(self, paths: Sequence[str], size: tuple[float, float] = None, masks_path: str = "blender/masks"):
         self._resize = identity if size is None else transforms.Resize(size)
+        self._masks_path = masks_path
 
         with Pool() as p:
             images = list(tqdm(p.imap(self._get_img, paths), total=len(paths)))
 
         self._images, self._masks = zip(*images)
+        # self._images = np.array(self._images, dtype="float32")
+        # self._masks = np.array(self._masks, dtype="int32")
 
         self._transforms = identity
         self._augmentations = transforms.Compose([transforms.ToDtype(torch.float32, scale=True)])
@@ -43,7 +47,7 @@ class BinaryKeyboardSegmentationDataset(Dataset):
 
     def _get_img(self, _img: str) -> tuple[torch.Tensor, torch.Tensor]:
         img = self._resize(self._to_image(Image.open(_img)))
-        mask = self._resize(self._to_image(Image.open(get_mask_path(_img)).convert("L")))
+        mask = self._resize(self._to_image(Image.open(get_mask_path(_img, self._masks_path)).convert("L")))
         return img, mask
 
 
