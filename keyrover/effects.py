5da647bed604257a9cafdf00905e7bc462f7e9cd
Bhavye Mathur <bhavyemathur@gmail.com>
2024-10-31 12:23:14 -0400

    Streamlined create_keyboard_dataset.py

---
+++ keyrover/effects.py
@@ -0,0 +1,157 @@
+import random
+import numpy as np
+
+from PIL import Image, ImageEnhance
+import cv2
+
+IMAGE_TYPE = np.ndarray | Image.Image
+
+
+def img_to_numpy(img: IMAGE_TYPE) -> np.ndarray:
+    if isinstance(img, np.ndarray):
+        return img
+    if isinstance(img, Image.Image):
+        return np.array(img)
+    raise TypeError("Can't convert unknown type image to numpy array")
+
+
+def img_to_PIL(img: IMAGE_TYPE) -> Image.Image:
+    if isinstance(img, np.ndarray):
+        return Image.fromarray(img)
+    if isinstance(img, Image.Image):
+        return img
+    raise TypeError("Can't convert unknown type image to PIL Image")
+
+
+def add_soft_shadow(img: IMAGE_TYPE) -> Image.Image:
+    img = img_to_PIL(img)
+
+    opacity = random.randint(50, 200)
+    thickness = random.randint(150, 400)
+    blur = max(30, int(random.gauss(40, 7.5))) * 2 + 1
+
+    overlay = np.zeros((*img.size, 4), dtype="uint8")
+    cv2.line(overlay, (0, 0), img.size, (0, 0, 0, opacity), thickness)
+    overlay = apply_random_affine(overlay)
+
+    overlay = cv2.GaussianBlur(overlay, (blur, blur), blur)
+    overlay = Image.fromarray(overlay)
+
+    return Image.alpha_composite(img, overlay)
+
+
+def add_hard_overlay(img: IMAGE_TYPE) -> Image.Image:
+    img = img_to_PIL(img)
+
+    thickness = random.randint(100, 400)
+    blur = max(5, int(random.gauss(25, 10)))
+
+    if random.random() > 0.5:
+        exp = max(0.5, random.gauss(2.5, 1))
+    else:
+        exp = max(0.5, random.gauss(0.75, 0.5))
+
+    alpha_mask = np.zeros((*img.size, 1), dtype="uint8")
+    cv2.line(alpha_mask, (0, 0), img.size, (255,), thickness)
+    alpha_mask = apply_random_affine(alpha_mask)
+
+    alpha_mask = cv2.blur(alpha_mask, (blur, blur))
+    alpha_mask = np.repeat(alpha_mask[:, :, np.newaxis], 4, axis=2)
+    alpha_mask = alpha_mask.astype("float32") / 255
+
+    bright_img = np.array(ImageEnhance.Brightness(img).enhance(exp))
+    img = alpha_mask * bright_img + (1 - alpha_mask) * img
+
+    return Image.fromarray(img.astype("uint8"))
+
+
+def add_chromatic_aberration(img: IMAGE_TYPE, strength: float = 0.01) -> np.ndarray:
+    img = img_to_numpy(img)
+
+    try:
+        b, g, r, _ = cv2.split(img)
+    except ValueError:
+        b, g, r = cv2.split(img)
+
+    height, width = img.shape[:2]
+
+    x, y = np.meshgrid(np.arange(width), np.arange(height))
+    center_x = width // 2
+    center_y = height // 2
+    distance = np.sqrt((x - center_x) ** 2 + (y - center_y) ** 2)
+
+    r_shifted = cv2.warpAffine(r, np.float32([[1, 0, strength * distance.max()], [0, 1, 0]]), (width, height))
+    b_shifted = cv2.warpAffine(b, np.float32([[1, 0, -strength * distance.max()], [0, 1, 0]]), (width, height))
+
+    return cv2.merge((r_shifted, g, b_shifted))
+
+
+def apply_random_affine(img: IMAGE_TYPE, scale_lims: tuple[float, float] = (0.25, 1.5)) -> np.ndarray:
+    img = img_to_numpy(img)
+
+    if len(img.shape) == 3:
+        w, h, _ = img.shape
+    elif len(img.shape) == 2:
+        w, h = img.shape
+    else:
+        raise ValueError("Image shape must have 2 or 3 dimensions")
+
+    angle = np.random.uniform(0, 360)
+    scale = np.random.uniform(*scale_lims)
+    M = cv2.getRotationMatrix2D((w / 2, h / 2), angle, scale)
+
+    tx = np.random.uniform(-w / 2, w / 2)
+    ty = np.random.uniform(-h / 2, h / 2)
+    T = np.float32([[1, 0, tx], [0, 1, ty]])
+
+    img = cv2.warpAffine(img, M, (w, h))
+    img = cv2.warpAffine(img, T, (w, h))
+
+    return img
+
+
+def apply_motion_blur(img: IMAGE_TYPE, theta: int, ksize: int) -> np.ndarray:
+    img = img_to_numpy(img)
+
+    blur_kernel = get_motion_blur_kernel(theta=theta, thickness=1, ksize=ksize)
+    img = cv2.filter2D(img, ddepth=-1, kernel=blur_kernel)
+    return img
+
+
+def apply_vignette(img: IMAGE_TYPE, sigma: int) -> np.ndarray:
+    img = img_to_numpy(img)
+    vignette_kernel = get_vignette_kernel(sigma, img.shape)
+    return (vignette_kernel * img).astype("uint8")
+
+
+# See https://medium.com/@itberrios6/how-to-apply-motion-blur-to-images-75b745e3ef17
+def get_motion_blur_kernel(theta: int, thickness: int = 1, ksize: int = 21) -> np.ndarray:
+    """ Obtains Motion Blur Kernel
+
+    @param theta - direction of blur
+    @param thickness - thickness of blur kernel line
+    @param ksize - size of blur kernel
+    """
+    c = ksize // 2
+
+    theta = np.radians(theta)
+    x = int(np.cos(theta) * 100)
+    y = int(np.sin(theta) * 100)
+
+    _kernel = np.zeros((ksize, ksize))
+    _kernel = cv2.line(_kernel, (c + x, c + y), (c, c), (1,), thickness)
+    return _kernel / _kernel.sum()
+
+
+def get_vignette_kernel(sigma: int, size: tuple[int, ...]) -> np.ndarray:
+    _kernel_x = cv2.getGaussianKernel(size[0], sigma)
+    _kernel_y = cv2.getGaussianKernel(size[1], sigma)
+    _kernel = _kernel_y * _kernel_x.T
+
+    _kernel /= _kernel.max()
+    return np.repeat(_kernel[:, :, np.newaxis], 3, axis=2)
+
+
+__all__ = ["add_soft_shadow", "add_hard_overlay", "add_chromatic_aberration",
+           "get_motion_blur_kernel", "get_vignette_kernel", "img_to_PIL", "img_to_numpy", "IMAGE_TYPE",
+           "apply_motion_blur", "apply_vignette", "apply_random_affine"]
--- scripts/create_keyboard_dataset.py
+++ scripts/create_keyboard_dataset.py
@@ -1,45 +1,50 @@
 import os
 import glob
 import shutil
+import random
 
 from tqdm import tqdm
 from multiprocessing import Pool
 
-from image_processing import *
-from PIL import ImageOps
+import numpy as np
+from PIL import Image, ImageEnhance
 
+from keyrover.effects import *
+from keyrover.paths import RESIZED_BACKGROUNDS, RAW_RENDERS, SEGMENTATION_DATASET
 
 _overlay_funcs = (add_hard_overlay, add_soft_shadow)
 
 
-def _get_img_with_random_background(path):
+def _get_img_with_random_background(path: str, backgrounds: str = RESIZED_BACKGROUNDS) -> Image.Image:
     image = Image.open(path)
+    background = random.choice(glob.glob(f"{backgrounds}/*.jpg"))
 
-    img = Image.open(random.choice(glob.glob("datasets/bg-20k-resized/*.jpg"))).convert("RGB")
+    img = Image.open(background)
+    img = img.convert("RGB")
     img = img.resize(image.size)
     img.paste(image, mask=image)
-    
+
     return img
 
 
-def _apply_motion_blur(img):
-    blur_kernel = get_motion_blur_kernel(theta=random.randint(0, 360), thickness=1, ksize=random.randint(4, 16))
-    img = cv.filter2D(np.array(img), ddepth=-1, kernel=blur_kernel)
-    return Image.fromarray(img)
+def _apply_random_motion_blur(img: IMAGE_TYPE) -> np.ndarray:
+    theta = random.randint(0, 360)
+    ksize = random.randint(4, 16)
+    return apply_motion_blur(img, theta=theta, ksize=ksize)
 
 
-def _apply_vignette(img):
-    vignette_kernel = get_vignette_kernel(random.randint(300, 600), img.size)
-    return (vignette_kernel * img).astype("uint8")
+def _apply_random_vignette(img: IMAGE_TYPE) -> np.ndarray:
+    sigma = random.randint(300, 600)
+    return apply_vignette(img, sigma=sigma)
 
 
-def _apply_random_image_enhance(img, min_exposure=0.4, min_contrast=0.2):
+def _apply_random_image_enhance(img: IMAGE_TYPE, min_exposure: float = 0.4, min_contrast: float = 0.2) -> Image.Image:
+    img = img_to_PIL(img)
+
     contrast, exposure, sharpness, saturation = np.random.normal(loc=1.0, scale=0.5, size=4)
     exposure = max(exposure, min_exposure)
     contrast = max(contrast, min_contrast)
 
-    if not isinstance(img, Image.Image):
-        img = Image.fromarray(img)
     img = ImageEnhance.Contrast(img).enhance(contrast)
     img = ImageEnhance.Brightness(img).enhance(exposure)
     img = ImageEnhance.Sharpness(img).enhance(sharpness)
@@ -48,89 +53,43 @@ def _apply_random_image_enhance(img, min_exposure=0.4, min_contrast=0.2):
     return img
 
 
-def _create_random_highlights(img):
+def _create_random_highlights(img: IMAGE_TYPE) -> Image.Image:
+    img = img_to_PIL(img)
     img = img.convert("RGBA")
 
-    for i in range(np.random.geometric(0.5, (1,))[0] - 1):
+    num_shadows = np.random.geometric(0.5, (1,))[0] - 1
+    for i in range(num_shadows):
         f = random.choice(_overlay_funcs)
         img = f(img)
 
     return img.convert("RGB")
 
 
-def _add_chromatic_abberation(img):
+def _add_chromatic_abberation(img: IMAGE_TYPE) -> np.ndarray:
     strength = random.uniform(0, 0.005)
-    img = np.array(img)
     img = add_chromatic_aberration(img, strength=strength)
-    return Image.fromarray(img)
+    return img
 
 
-def create_random_image_v1(n):
-    global OUTPUT_PATH
-    OUTPUT_PATH = "datasets/segmentation/v1"
-
-    path = random.choice(os.listdir("datasets/segmentation/renders"))
-
+def create_random_image(path):
     output = _get_img_with_random_background(path)
-    output = _apply_motion_blur(output)
-    output = _apply_vignette(output)
-    output = _apply_random_image_enhance(output)
 
-    output_path = f"{OUTPUT_PATH}/{path[-7:-4]}_{n}.png"
-    output.save(output_path)
-
-
-def create_random_image_v2(n):
-    global OUTPUT_PATH
-    OUTPUT_PATH = "datasets/segmentation/v2"
-
-    path = random.choice(os.listdir("datasets/segmentation/renders"))
-
-    output = _get_img_with_random_background(path)
-    output = _create_random_highlights(output)
-    output = _apply_motion_blur(output)
-    output = _apply_vignette(output)
-    output = _apply_random_image_enhance(output)
-
-    output_path = f"{OUTPUT_PATH}/{path[-7:-4]}_{n}.png"
-    output.save(output_path)
-
-
-def create_image_dataset_v1(N):
-    global IMAGES_PATH
-
-    shutil.rmtree(f"assets/keyboards_v{DATASET_VERSION}")
-    os.mkdir(f"assets/keyboards_v{DATASET_VERSION}")
-
-    IMAGES_PATH = "datasets/segmentation/renders"
-
-    with Pool() as p:
-        r = list(tqdm(p.imap(create_random_image_v2, range(N)), total=N))
-
-
-def create_random_image_v3(p, flip=False):
-    output = _get_img_with_random_background(p)
-    if flip:
-        output = ImageOps.mirror(output)
     output = _add_chromatic_abberation(output)
     output = _create_random_highlights(output)
-    output = _apply_motion_blur(output)
+    output = _apply_random_motion_blur(output)
     # output = _apply_random_image_enhance(output, min_exposure=1, min_contrast=0.5)
+    output = Image.fromarray(output)
 
-    output_path = f"datasets/segmentation/v3/{os.path.basename(p)}"
+    output_path = f"{SEGMENTATION_DATASET}/{os.path.basename(path)}"
     output.save(output_path)
 
 
 if __name__ == "__main__":
     DATASET_VERSION = 3
 
-    BACKGROUNDS = glob.glob("datasets/bg-20k-resized/")
-    IMAGES_PATH = "blender/renders/*.png"
-    OUTPUT_PATH = "datasets/segmentation/v3"
-
-    shutil.rmtree(OUTPUT_PATH)
-    os.mkdir(OUTPUT_PATH)
+    shutil.rmtree(SEGMENTATION_DATASET)
+    os.mkdir(SEGMENTATION_DATASET)
 
     with Pool() as p:
-        paths = glob.glob(IMAGES_PATH)
-        r = list(tqdm(p.imap(create_random_image_v3, paths), total=len(paths)))
+        paths = glob.glob(f"{RAW_RENDERS}/*.png")
+        r = list(tqdm(p.imap(create_random_image, paths), total=len(paths)))
