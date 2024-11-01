0dc1947fca98b8cd763e667d5114b0d4960e43e1
Bhavye Mathur <bhavyemathur@gmail.com>
2024-11-01 12:41:35 -0400

    Fixed bug with get_mask_path function

--- keyrover/datasets/internal.py
+++ keyrover/datasets/internal.py
@@ -2,7 +2,7 @@ import os
 
 
 def get_mask_path(path: str, base: str = "blender/masks") -> str:
-    return f"{base}/{os.path.basename(path)}"
+    return f"{base}/{os.path.basename(path).removesuffix('.jpg')}.png"
 
 
 __all__ = ["get_mask_path"]
