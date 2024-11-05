838a245c993014ba05352ea385479d49bd00dbaa
Bhavye Mathur <bhavyemathur@gmail.com>
2024-11-05 14:30:59 -0500

    Added texcoord dataset

--- keyrover/datasets/__init__.py
+++ keyrover/datasets/__init__.py
@@ -1,5 +1,8 @@
 from .keyboard_dataset import *
 from .binary_dataset import *
 from .texcoords_dataset import *
-from .mutliclass_image import *
+
+from .multiclass_image import *
+from .texcoord_image import *
+
 from .util import *
---
+++ keyrover/datasets/texcoord_image.py
@@ -0,0 +1,31 @@
+from keyrover import *
+from keyrover.color import image_color
+
+from .multiclass_image import MulticlassKeyboardImage
+
+
+class TexCoordKeyboardImage(MulticlassKeyboardImage):
+    def __init__(self, path: str) -> None:
+        super().__init__(path)
+
+        self.texcoord_path = f"{RAW_TEXCOORDS}/{self.filename}.png"
+        self.texcoords = Image.open(self.texcoord_path).convert("RGB")
+        self.texcoords = img_to_numpy(self.texcoords)
+
+        self.key_texcoords: dict[str, list[float]] = {}
+
+    def show(self) -> None:
+        imshow(self.image, self.texcoords)
+
+    def extract_key_texcoords(self) -> None:
+        self.key_texcoords = {"U": [], "V": []}
+
+        for label, rect in zip(self.labels, self.rects):
+            crop = self.crop(*rect, attr="texcoords")
+            r, g, _ = image_color(crop, reduce="mean")
+
+            self.key_texcoords["U"].append(r)
+            self.key_texcoords["V"].append(g)
+
+
+__all__ = ["TexCoordKeyboardImage"]
