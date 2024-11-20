8ec8108625dea2a2e336bac25c5d06f678b34540
Bhavye Mathur <bhavyemathur@gmail.com>
2024-11-19 19:52:58 -0500

    More work with corner prediction

--- keyrover/datasets/texcoords_dataset.py
+++ keyrover/datasets/texcoords_dataset.py
@@ -18,9 +18,10 @@ from .abstract import KeyboardDatasetBase
 class KeyboardTexCoordsDataset(KeyboardDatasetBase):
     _to_image = transforms.ToImage()
 
-    def __init__(self, paths: Sequence[str], size: tuple[float, float] = None):
+    def __init__(self, paths: Sequence[str], size: tuple[float, float] = None, norm: bool = True):
         super().__init__()
         self._resize = identity if size is None else transforms.Resize(size)
+        self._norm = norm
 
         with Pool() as p:
             images = list(tqdm(p.imap(self._get_img, paths), total=len(paths)))
@@ -33,7 +34,9 @@ class KeyboardTexCoordsDataset(KeyboardDatasetBase):
 
         mask_path = f"{RAW_TEXCOORDS}/{os.path.basename(path).removesuffix('.jpg')}.png"
         target = Image.open(mask_path).convert("RGB")
-        target = (self._resize(self._to_image(target))[:2].float() / 128) - 1
+        target = self._resize(self._to_image(target))[:2].float()
+        if self._norm:
+            target = (target / 128) - 1
         return img, target
 
 
