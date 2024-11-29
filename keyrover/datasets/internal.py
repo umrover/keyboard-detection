a2d70fcc9ccd2844aebaea48b8cf1a8bc7d9f284
Bhavye Mathur <bhavyemathur@gmail.com>
2024-11-28 21:05:59 -0500

    Fixed dataset-normalization.ipynb mean & std calculation

--- keyrover/datasets/internal.py
+++
@@ -1,8 +0,0 @@
-import os
-
-
-def get_mask_path(path: str, base: str = "blender/masks") -> str:
-    return f"{base}/{os.path.basename(path).removesuffix('.jpg')}.png"
-
-
-__all__ = ["get_mask_path"]
